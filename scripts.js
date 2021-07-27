c6cdb32aff6a988598b3a79b9615c2d753fb5c59
vbussie <86642827+vbussie@users.noreply.github.com>
2021-07-26 21:37:50 -0500

    Added Developer Information

--- scripts.js
+++ scripts.js
@@ -1,2 +1,5 @@
+//Developer: Vanessa Bussie
+// Java Lab
+
 // document.write("footer class='text-light bg-dark'>Thank you for again!</footer>")
 document.getElementById("theFooter").innerHTML="<footer class='text-light bg-dark'>Thank you again!</footer>";
